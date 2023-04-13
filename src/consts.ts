48f4124dc1ab1a657b733ea3b5f9872dc2482d17
Michał Rosa <michalrosa17@gmail.com>
2023-04-13 17:09:11 +0200

    add techIcons to consts

--- src/consts.ts
+++ src/consts.ts
@@ -1,7 +1,8 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = "Michal Rosa. I'm web developer. That's my personal website where I want to share some knowledge.";
+export const SITE_TITLE =
+  "Michal Rosa. I'm web developer. That's my personal website where I want to share some knowledge.";
 export const SITE_DESCRIPTION = "Welcome to my website!";
 
 export const data = {
@@ -41,60 +42,61 @@ export const data = {
     },
   ],
   techIcons: [
-        {
-          src: 'postgresql',
-          alt: 'postgresql',
-          link: 'https://www.postgresql.org/',
-        },
-        {
-          src: 'express',
-          alt: 'express',
-          link: 'https://expressjs.com/',
-        },
-        {
-          src: 'react',
-          alt: 'react',
-          link: 'https://reactjs.org/',
-        },
-        {
-          src: 'node',
-          alt: 'node.js',
-          link: 'https://nodejs.org/',
-        },
-        {
-          src: 'typeorm',
-          alt: 'typeorm',
-          link: 'https://typeorm.io/',
-        },
-        {
-          src: 'typescript',
-          alt: 'typescript',
-          link: 'https://www.typescriptlang.org/',
-        },
-        {
-          src: 'vue',
-          alt: 'vue.js',
-          link: 'https://vuejs.org/',
-        },
-        {
-          src: 'firebase',
-          alt: 'firebase',
-          link: 'https://firebase.google.com/',
-        },
-        {
-          src: 'gcp',
-          alt: 'google cloud platform',
-          link: 'https://cloud.google.com/',
-        },
-        {
-          src: 'github',
-          alt: 'github',
-          link: 'https://github.com/',
-        },
-        {
-          src: 'ghactions',
-          alt: 'github actions',
-          link: 'https://github.com/features/actions',
-        },
-      ],
+    {
+      src: "postgresql",
+      alt: "postgresql",
+      link: "https://www.postgresql.org/",
+    },
+    {
+      src: "express",
+      alt: "express",
+      link: "https://expressjs.com/",
+    },
+    {
+      src: "react",
+      alt: "react",
+      link: "https://reactjs.org/",
+    },
+    {
+      src: "node",
+      alt: "node.js",
+      link: "https://nodejs.org/",
+    },
+
+    {
+      src: "typeorm",
+      alt: "typeorm",
+      link: "https://typeorm.io/",
+    },
+    {
+      src: "typescript",
+      alt: "typescript",
+      link: "https://www.typescriptlang.org/",
+    },
+    {
+      src: "vue",
+      alt: "vue.js",
+      link: "https://vuejs.org/",
+    },
+    {
+      src: "firebase",
+      alt: "firebase",
+      link: "https://firebase.google.com/",
+    },
+    {
+      src: "gcp",
+      alt: "google cloud platform",
+      link: "https://cloud.google.com/",
+    },
+    {
+      src: "github",
+      alt: "github",
+      link: "https://github.com/",
+    },
+    {
+      src: "ghactions",
+      alt: "github actions",
+      link: "https://github.com/features/actions",
+    },
+  ],
 };
