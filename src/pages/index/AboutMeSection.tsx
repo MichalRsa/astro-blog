248be933b9192ba2130d8a83787c18311b108d1b
MichalRsa <michalrosa17@gmail.com>
2023-07-01 12:02:30 +0200

    Fix text in AboutMeSection component

--- src/pages/index/AboutMeSection.tsx
+++ src/pages/index/AboutMeSection.tsx
@@ -5,15 +5,14 @@ export const AboutMeSection = (): ReactElement => {
   return (
     <section id="about-me">
       <SectionTitle title="About Me" />
-      <p>Self-taught Web Developer,</p>
-      <br />
       <p>
-        just enjoying writing code. I worked on several commercial projects with
-        customers from Great Britain and Spain, developing both frontend and
-        backend parts. Most proficient with React and its environment.
-        Self-reliant at work, familiar with macOS and Linux. Confident in
-        working with git/GitHub, CLI, CI/CD tools, and Cloud Computing services.
-        Striving to stay up to date with bleeding edge technologies.
+        <b>Self-taught Web Developer</b>, just enjoying writing code. I worked
+        on several commercial projects with customers from Great Britain and
+        Spain, developing both frontend and backend parts. Most proficient with
+        React and its environment. Self-reliant at work, familiar with macOS and
+        Linux. Confident in working with git/GitHub, CLI, CI/CD tools, and Cloud
+        Computing services. Striving to stay up to date with bleeding edge
+        technologies.
       </p>
     </section>
   );
