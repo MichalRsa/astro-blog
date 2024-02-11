8b5f13be4375020ceb80c4633219618312931482
MichalRsa <michalrosa17@gmail.com>
2024-02-11 10:52:46 +0100

    Add new header - this thing is a mess, it needs to be changed

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,20 +1,20 @@
-import { type ReactElement, useState } from "react";
-import { Button } from "../components/Button.tsx";
-import { useMediaQuery } from "react-responsive";
-import clsx from "clsx";
+import { type ReactElement, useState } from 'react';
+import { Button } from '../components/Button.tsx';
+import { useMediaQuery } from 'react-responsive';
+import clsx from 'clsx';
 const links = [
   {
-    label: "about me",
-    url: "/",
+    label: 'about me',
+    url: '/',
   },
   {
-    label: "blog",
-    url: "/blog/",
+    label: 'blog',
+    url: '/blog/',
   },
 ];
 
 export const Header = (): ReactElement => {
-  const isMobile = useMediaQuery({ query: "(max-width: 640px)" });
+  const isMobile = useMediaQuery({ query: '(max-width: 640px)' });
   const [showMobileMenu, setShowMobileMenu] = useState(false);
 
   const handleClick = (): void => {
@@ -23,27 +23,27 @@ export const Header = (): ReactElement => {
 
   return (
     <header>
-      <nav className="fixed flex flex-col-reverse max-lg:bottom-0 lg:top-0 w-full z-50">
-        <div className="z-50 bg-secondary-500 text-ternary-500 flex justify-between sm:justify-center lg:justify-between items-center px-4 py-2 sm:px-12 sm:py-5  box-border">
+      <nav className="fixed z-50 flex w-full flex-col-reverse max-lg:bottom-0 lg:top-0">
+        <div className="z-50 box-border flex items-center justify-between bg-secondary-500 px-4 py-2 text-ternary-500 sm:justify-center sm:px-12 sm:py-5  lg:justify-between">
           <h2 className="text-3xl max-lg:hidden">
             Michał Rosa | Frontend Developer
           </h2>
           {isMobile && (
             <button
-              className={clsx("hamburger-menu", showMobileMenu && "active")}
+              className={clsx('hamburger-menu', showMobileMenu && 'active')}
               onClick={() => {
                 handleClick();
               }}
             >
               <span
-                className={clsx("hamburger-menu", showMobileMenu && "active")}
+                className={clsx('hamburger-menu', showMobileMenu && 'active')}
               />
             </button>
           )}
-          <ul className="flex items-center z-50">
+          <ul className="z-50 flex items-center">
             {!isMobile &&
               links.map((link) => (
-                <li className="text-xl mx-2 capitalize" key={link.label}>
+                <li className="mx-2 text-xl capitalize" key={link.label}>
                   <a
                     href={link.url}
                     className="p-3 hover:underline hover:decoration-2 hover:underline-offset-8"
@@ -52,11 +52,11 @@ export const Header = (): ReactElement => {
                   </a>
                 </li>
               ))}
-            <li className="text-xl mx-2">
+            <li className="mx-2 text-xl">
               <Button
                 text="Let's work together"
                 fontSize="text-xl"
-                classes={clsx(isMobile && "py-1 px-6")}
+                classes={clsx(isMobile && 'py-1 px-6')}
               />
             </li>
           </ul>
@@ -64,14 +64,14 @@ export const Header = (): ReactElement => {
         {isMobile && (
           <ul
             className={clsx(
-              showMobileMenu && "active",
-              "dropdown",
-              "bg-secondary-400 text-center"
+              showMobileMenu && 'active',
+              'dropdown',
+              'bg-secondary-400 text-center',
             )}
           >
             {links.map((link) => (
               <li
-                className="text-xl p-3 capitalize text-ternary-500"
+                className="p-3 text-xl capitalize text-ternary-500"
                 key={link.label}
               >
                 <a
