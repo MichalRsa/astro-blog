a324bd4014e1a84494763e72f3c7e638985bd36c
MichalRsa <michalrosa17@gmail.com>
2024-02-12 15:20:11 +0100

    Add DesignCodingSection

--- src/components/SectionTitle.tsx
+++ src/components/SectionTitle.tsx
@@ -1,11 +1,31 @@
+import clsx from "clsx";
 import type { ReactElement } from "react";
 
 interface SectionTitleProps {
   title: string;
+  color?: "primary" | "secondary";
+  underlineFullWidth?: boolean
 }
 
-export const SectionTitle = ({ title }: SectionTitleProps): ReactElement => (
-  <h2 className="mb-8 text-2xl md:text-4xl underline decoration-2 underline-offset-8">
-    {title}
-  </h2>
-);
+export const SectionTitle = ({
+  title,
+  color = "secondary",
+  underlineFullWidth = false
+}: SectionTitleProps): ReactElement => {
+  const titleColor = {
+    primary: "border-primary-500 text-primary-500",
+    secondary: "border-secondary-500 text-secondary-500",
+  };
+
+  return (
+    <h2
+      className={clsx(
+        "mb-8  text-2xl md:text-4xl",
+        underlineFullWidth ? 'border-b-2 pb-1' : 'underline decoration-2 underline-offset-8',
+        titleColor[color],
+      )}
+    >
+      {title}
+    </h2>
+  );
+};
