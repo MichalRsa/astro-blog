8b74210cc7f814c5439fc91b2d6a3f3a060459a9
MichalRsa <michalrosa17@gmail.com>
2024-03-16 12:05:21 +0100

    Adds meta title and description for each page

--- src/consts.ts
+++ src/consts.ts
@@ -1,17 +1,24 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-const BASE_TITLE = "Michał Rosa Fullstack developer | ";
+const BASE_TITLE = "Michał Rosa Frontend developer | ";
 
 export const TITLE = {
   HOME: BASE_TITLE + "Home",
   BLOG: BASE_TITLE + "Blog",
+  FREELANCING: BASE_TITLE + "Freelancing",
+  CONTACT_ME: BASE_TITLE + "Contact me",
+
   BLOG_POST: (description: string) => BASE_TITLE + description,
 };
 
 export const DESCRIPTION = {
-  HOME: "Michał Rosa is a Fullstack developer, with commercial experience in making web apps using technologies like React.js, Node.js, and Express.js",
-  BLOG: "Welcome to my blog! As a web developer proficient in JavaScript, TypeScript, React.js and Node.js. I share insightful articles, tutorials, and tips on these topics. Stay updated with the latest web development trends and learn how to leverage these technologies to build robust and efficient applications.",
+  HOME: "Michał Rosa - Frontend developer, with commercial experience in making web apps using technologies like React.js, Node.js, and Express.js",
+  BLOG: "Welcome to my blog! As a web developer proficient in JavaScript, TypeScript, React.js and Node.js. I share insightful articles, tutorials, and tips on these topics.",
+  FREELANCING:
+    "I build custom, responsive, acessible and editable webistes for small business.",
+  CONTACT_ME:
+    "Contact me if you need developer for your team or freelancer to make you website.",
 };
 
 export const data = {
