aa34c1504abdc257090d6f0c1e966df0ec53937a
MichalRsa <michalrosa17@gmail.com>
2024-03-23 06:25:49 +0100

    Instals partytown, adds google analytics

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,30 +1,34 @@
 import { defineConfig } from "astro/config";
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
-
 import react from "@astrojs/react";
 import tailwind from "@astrojs/tailwind";
 import rehypePrettyCode from "rehype-pretty-code";
-
+import partytown from "@astrojs/partytown";
 const options = {
   onVisitLine(node) {
     if (node.children.length === 0) {
-      node.children = [{ type: "text", value: " " }];
+      node.children = [{
+        type: "text",
+        value: " "
+      }];
     }
   },
   onVisitHighlightedLine(node) {
     if (node.properties.className === undefined) node.properties.className = [];
     node.properties.className?.push("highlighted");
-  },
+  }
 };
+
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://michalrosa.dev",
-  integrations: [mdx(), sitemap(), react(), tailwind()],
+  integrations: [mdx(), sitemap(), react(), tailwind(), partytown()],
   markdown: {
-    syntaxHighlight: false, // Disable syntax built-in syntax hightlighting from astro
+    syntaxHighlight: false,
+    // Disable syntax built-in syntax hightlighting from astro
     extendDefaultPlugins: true,
-    rehypePlugins: [[rehypePrettyCode, options]],
-  },
+    rehypePlugins: [[rehypePrettyCode, options]]
+  }
 });
