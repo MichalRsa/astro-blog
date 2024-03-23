347dbb36e52a0b26d40efeda29786e8c69a776be
MichalRsa <michalrosa17@gmail.com>
2024-03-23 09:03:33 +0100

    Test google analytics view transition

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,27 +8,35 @@ import partytown from "@astrojs/partytown";
 const options = {
   onVisitLine(node) {
     if (node.children.length === 0) {
-      node.children = [{
-        type: "text",
-        value: " "
-      }];
+      node.children = [
+        {
+          type: "text",
+          value: " ",
+        },
+      ];
     }
   },
   onVisitHighlightedLine(node) {
     if (node.properties.className === undefined) node.properties.className = [];
     node.properties.className?.push("highlighted");
-  }
+  },
 };
-
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://michalrosa.dev",
-  integrations: [mdx(), sitemap(), react(), tailwind(), partytown()],
+  integrations: [
+    mdx(),
+    sitemap(),
+    react(),
+    tailwind(),
+    partytown({ config: { forward: ["dataLayer.push"] } }),
+  ],
   markdown: {
     syntaxHighlight: false,
     // Disable syntax built-in syntax hightlighting from astro
     extendDefaultPlugins: true,
-    rehypePlugins: [[rehypePrettyCode, options]]
-  }
+    rehypePlugins: [[rehypePrettyCode, options]],
+  },
 });
+
